6524e42af7a6216f6f0f5ef1d93da6aff767f078
Keiichi Tabata <tabata.keiichi@gmail.com>
2024-04-14 00:35:39 +0900

    build: Apple

--- web/update-version.sh
+++ web/update-version.sh
@@ -23,7 +23,7 @@ NOTE_EN=`cat ../ChangeLog | awk '/BEGIN-LATEST-EN/,/END-LATEST-EN/' | tail -n +2
 cat dl/index.html | \
 $SED -e "s|.*LATEST-VERSION-AND-DATE.*|<!-- LATEST-VERSION-AND-DATE -->最新版 Polaris Engine $VERSION \($DATE\):<br>|" \
      -e "s|.*LATEST-EXE.*|<!-- LATEST-EXE --><a href=\"/dl/polaris-engine-$VERSION.exe\">Windows版 Polaris Engine</a><br>|" \
-     -e "s|.*LATEST-DMG.*|<!-- LATEST-DMG -->Mac版 Polaris Engine (Appleが公証中)<br>|" \
+     -e "s|.*LATEST-DMG.*|<!-- LATEST-DMG --><a href=\"/dl/polaris-engine-$VERSION.dmg\">Mac版 Polaris Engine</a><br>|" \
  > tmp
 cat tmp | awk '/.*DOCTYPE html.*/,/.*LATEST-RELEASE-NOTE.*/' > before
 cat tmp | awk '/.*LATEST-RELEASE-NOTE.*/,/\<\/html\>/' | tail -n +2 > after
@@ -37,7 +37,7 @@ rm -f before after new tmp
 cat en/dl/index.html | \
 $SED -e "s|.*LATEST-VERSION-AND-DATE.*|<!-- LATEST-VERSION-AND-DATE -->Latest $VERSION \($DATE\):<br>|" \
      -e "s|.*LATEST-EXE.*|<!-- LATEST-EXE --><a href=\"/dl/polaris-engine-$VERSION.exe\">Download Polaris Engine for Windows</a><br>|" \
-     -e "s|.*LATEST-DMG.*|<!-- LATEST-DMG -->Download Polaris Engine for Mac (Apple is now notalizing)<br>|" \
+     -e "s|.*LATEST-DMG.*|<!-- LATEST-DMG --><a href=\"/dl/polaris-engine-$VERSION.exe\">Download Polaris Engine for Mac</a><br>|" \
   > tmp
 cat tmp | awk '/.*DOCTYPE html.*/,/.*LATEST-RELEASE-NOTE.*/' > before
 cat tmp | awk '/.*LATEST-RELEASE-NOTE.*/,/\<\/html\>/' | tail -n +2 > after
