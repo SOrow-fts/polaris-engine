19311144eba6d405078cbdbc45f19800cd18df1d
Keiichi Tabata <ktabata@polaris-engine.com>
2024-05-07 12:59:47 +0900

    Fixed Android automatic build

--- build/engine-android/build.bat
+++ build/engine-android/build.bat
@@ -1,4 +1,4 @@
-@ech off
+@echo off
 
 del /Q /S microsoft-jdk-17.0.11-windows-x64
 del /Q /S commandlinetools-win-11076708_latest
@@ -7,7 +7,13 @@ set CURRENT_DIR=%~dp0
 
 curl -L -O https://aka.ms/download-jdk/microsoft-jdk-17.0.11-windows-x64.zip
 call powershell -command "Expand-Archive microsoft-jdk-17.0.11-windows-x64.zip"
-
 set JAVA_HOME=%CURRENT_DIR%microsoft-jdk-17.0.11-windows-x64\jdk-17.0.11+9
 
+curl -L -O https://dl.google.com/android/repository/commandlinetools-win-11076708_latest.zip
+call powershell -command "Expand-Archive commandlinetools-win-11076708_latest.zip"
+
+set ANDROID_SDK_ROOT=%CURRENT_DIR%commandlinetools-win-11076708_latest\commandline-tools
+./cmdline-tools/bin/sdkmanager "platforms;android-34 --sdk_root=%ANDROID_SDK_ROOT%
+
 gradlew.bat build
+gradlew.bat --stop
