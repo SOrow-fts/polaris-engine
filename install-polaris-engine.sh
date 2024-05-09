e9f8d49e2e9e563c66859d6e39bcafc4c78ab752
Keiichi Tabata <ktabata@polaris-engine.com>
2024-05-09 12:06:34 +0900

    build: fixed the install script

--- install-polaris-engine.sh
+++ install-polaris-engine.sh
@@ -11,7 +11,7 @@ if [ ! -z "`echo $UNAME | grep Ubuntu`" ]; then
     sudo apt update;
     sudo apt install build-essential git wget unzip zlib1g-dev libpng-dev libjpeg9-dev libbz2-dev libwebp-dev libogg-dev libvorbis-dev libfreetype-dev libasound2-dev libx11-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libxpm-dev mesa-common-dev cmake qt6-base-dev qt6-multimedia-dev libqt6core6 libqt6gui6 libqt6widgets6 libqt6opengl6-dev libqt6openglwidgets6 libqt6multimedia6 libqt6multimediawidgets6 libbrotli-dev
     git clone https://github.com/ktabata/polaris-engine;
-    cd polaris-engine-master;
+    cd polaris-engine;
     ./configure;
     make;
     sudo make install;
@@ -26,7 +26,7 @@ if [ ! -z "`echo $UNAME | grep Debian`" ]; then
     sudo apt update;
     sudo apt install build-essential git wget unzip zlib1g-dev libpng-dev libjpeg-dev libbz2-dev libwebp-dev libogg-dev libvorbis-dev libfreetype-dev libasound2-dev libx11-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libxpm-dev mesa-common-dev cmake qt6-base-dev qt6-multimedia-dev libqt6core6 libqt6gui6 libqt6widgets6 libqt6opengl6-dev libqt6openglwidgets6 libqt6multimedia6 libqt6multimediawidgets6 libbrotli-dev
     git clone https://github.com/ktabata/polaris-engine;
-    cd polaris-engine-master;
+    cd polaris-engine;
     ./configure;
     make;
     sudo make install;
