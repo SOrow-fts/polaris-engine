2fdc45bcca12b1c58335dba54d2deb54750c18fe
Keiichi Tabata <ktabata@polaris-engine.com>
2024-05-11 20:54:00 +0900

    polaris-novel.app: tab to space

--- tools/polaris-novel.app/index.php
+++ tools/polaris-novel.app/index.php
@@ -10,19 +10,19 @@ function init()
 
     $id = "" . time();
 
-		$account = htmlspecialchars($_POST['title']);
+    $account = htmlspecialchars($_POST['title']);
     if ($account == '') {
         $GLOBALS['error_msg'] = 'アカウントを入力してください。';
         return;
     }
 
-		$title = htmlspecialchars($_POST['title']);
+    $title = htmlspecialchars($_POST['title']);
     if ($title == '') {
         $GLOBALS['error_msg'] = 'タイトルを入力してください。';
         return;
     }
 
-		$explanation = htmlspecialchars($_POST['explanation']);
+    $explanation = htmlspecialchars($_POST['explanation']);
     if ($explanation == '') {
         $GLOBALS['error_msg'] = '説明を入力してください。';
         return;
@@ -45,9 +45,9 @@ function init()
         copy($_FILES['preview']['tmp_name'], 'run/' . $id . '/preview.jpg');
         copy($_FILES['data01arc']['tmp_name'], 'run/' . $id . '/data01.arc');
         copy('base/index.html', 'run/' . $id . '/index.html');
-				copy('base/index.js', 'run/' . $id . '/index.js');
+                copy('base/index.js', 'run/' . $id . '/index.js');
         copy('base/index.wasm', 'run/' . $id . '/index.wasm');
-				exec("sed -e 's|<title>A Polaris Engine Exported Game</title>|" .
+                exec("sed -e 's|<title>A Polaris Engine Exported Game</title>|" .
              '<title>A Polaris Engine Exported Game</title>' .
              '<meta name="twitter:card" content="player">' .
              '<meta name="twitter:site" content="' . $account . '">' .
@@ -57,7 +57,7 @@ function init()
              '<meta name="twitter:player" content="https://polaris-novel.app/run/' . $id . '/">' .
              '<meta name="twitter:player:width" content="1280">' .
              '<meta name="twitter:player:height" content="720">' . "|g' " .
-						 ' -i run/' . $id . '/index.html');
+                         ' -i run/' . $id . '/index.html');
     } catch (Exception $e) {
         echo($e->getMessage());
         return;
@@ -88,9 +88,9 @@ init();
 <body>
 <h1 class="space">Polaris Engine SNSタイムライン 連携</h1>
 <p>
-	Xのタイムラインにゲームを埋め込むためのURLを生成します。<br>
-	ゲームは7日後に消えます。<br>
-	XのAPI連携は行いませんので安心してご利用ください。
+  Xのタイムラインにゲームを埋め込むためのURLを生成します。<br>
+  ゲームは7日後に消えます。<br>
+  XのAPI連携は行いませんので安心してご利用ください。
 </p>
 
 <h2>アップロード</h2>
@@ -99,23 +99,23 @@ init();
 <p style="color:blue;"><?php on_succeed(); ?></p>
 
 <form method="POST" action="https://polaris-novel.app/" enctype="multipart/form-data">
-    <label>Xアカウント名 (@を含む):</label><br>
-    <input type="text" id="account" name="account"><br>
-		<br>
-    <label>ゲームタイトル:</label><br>
-    <input type="text" id="title" name="title"><br>
-		<br>
-    <label>説明文:</label><br>
-    <input type="text" id="explanation" name="explanation"><br>
-		<br>
-    <label>プレビュー画像(JPEG):</label><br>
-    <input type="file" id="preview" name="preview" accept="image/jpeg"><br>
-		<br>
-    <label>data01.arc (30MB以内):</label><br>
-    <input type="file" id="data01arc" name="data01arc" accept=".arc"><br>
-    <br>
-		<br>
-    <input type="submit" value="アップロードを実行する" name="submit">
+  <label>Xアカウント名 (@を含む):</label><br>
+  <input type="text" id="account" name="account"><br>
+  <br>
+  <label>ゲームタイトル:</label><br>
+  <input type="text" id="title" name="title"><br>
+  <br>
+  <label>説明文:</label><br>
+  <input type="text" id="explanation" name="explanation"><br>
+  <br>
+  <label>プレビュー画像(JPEG):</label><br>
+  <input type="file" id="preview" name="preview" accept="image/jpeg"><br>
+  <br>
+  <label>data01.arc (30MB以内):</label><br>
+  <input type="file" id="data01arc" name="data01arc" accept=".arc"><br>
+  <br>
+  <br>
+  <input type="submit" value="アップロードを実行する" name="submit">
 </form>
 
 <br>
@@ -123,9 +123,9 @@ init();
 <br>
 <br>
 <p>
-	SNSのタイムラインへの投稿以外に利用しないでください。<br>
-	ゲームの一般公開に利用しないでください。<br>
-	表現が不適切なコンテンツをみつけた際は core@polaris-engine.com へご連絡ください。
+  SNSのタイムラインへの投稿以外に利用しないでください。<br>
+  ゲームの一般公開に利用しないでください。<br>
+  表現が不適切なコンテンツをみつけた際は core@polaris-engine.com へご連絡ください。
 </p>
 
 </body>
